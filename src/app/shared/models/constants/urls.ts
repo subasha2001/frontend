55f0724d542502ea330a0efb3be14ca3330d0acc
Subash <subashayyanar1@gmail.com>
2024-12-03 14:36:24 +0530

    new backend js

--- src/app/shared/models/constants/urls.ts
+++ src/app/shared/models/constants/urls.ts
@@ -1,6 +1,6 @@
 // export const BASE_URL = 'https://backend-gpj.onrender.com';
-export const BASE_URL = 'http://13.61.105.213:3001';
-// export const BASE_URL = 'http://localhost:3001';
+// export const BASE_URL = 'http://13.61.105.213:3001';
+export const BASE_URL = 'http://localhost:3002';
 
 export const PRODUCTS_URL = BASE_URL + '/api/products';
 export const PRODUCTS_METALTYPES_URL = PRODUCTS_URL + '/metalType';
